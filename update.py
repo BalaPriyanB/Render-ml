4c771397be1945bd21908f21a581f8053d51ee8b
BalaPriyanB <117967016+BalaPriyanB@users.noreply.github.com>
2024-06-21 02:22:29 +0530

    Update update.py

--- update.py
+++ update.py
@@ -52,7 +52,7 @@ if len(DATABASE_URL) == 0:
 if DATABASE_URL is not None:
     try:
         conn = MongoClient(DATABASE_URL, server_api=ServerApi("1"))
-        db = conn.mltb
+        db = conn.bbots
         old_config = db.settings.deployConfig.find_one({"_id": bot_id})
         config_dict = db.settings.config.find_one({"_id": bot_id})
         if old_config is not None:
